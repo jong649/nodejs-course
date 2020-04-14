e5686a5cf27c1036cac7371074be2f7bedaa175c
Jon <jong649@gmail.com>
2020-04-14 13:48:50 -0700

    use yargs to set up commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,45 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv[2];
+yargs.version("1.1.0");
 
-console.log(process.argv);
+//Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note!");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding note!");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+//Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Removing the note!");
+  },
+});
+
+//Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: () => {
+    console.log("Reading the note");
+  },
+});
+
+//Create list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: () => {
+    console.log("Listing all notes");
+  },
+});
+
+// add, remove, read, list
+
+console.log(yargs.argv);
