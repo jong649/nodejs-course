f3da45a188cc660004ac5302aa0b92c8878ba209
Jon <jong649@gmail.com>
2020-04-13 23:38:36 -0700

    add validator package and test some methods

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,8 @@
+const validator = require("validator");
 const getNotes = require("./notes.js");
 
 const notes = getNotes();
+
 console.log(notes);
+
+console.log(validator.isURL("https/mead.io"));
