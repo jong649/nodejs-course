32bd4eeceff56f3583f92b98065ba937e2d95ff0
Jon <jong649@gmail.com>
2020-04-14 14:03:06 -0700

    set up add command to require title and body

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,8 +8,21 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: () => {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
   },
 });
 
@@ -35,6 +48,7 @@ yargs.command({
 yargs.command({
   command: "list",
   describe: "List all notes",
+  builder: {},
   handler: () => {
     console.log("Listing all notes");
   },
@@ -42,4 +56,5 @@ yargs.command({
 
 // add, remove, read, list
 
-console.log(yargs.argv);
+yargs.parse();
+//console.log(yargs.argv);
