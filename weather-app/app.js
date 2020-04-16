b7e5754c458e156ab61e66c4069befd7dbea4e18
Jon <jong649@gmail.com>
2020-04-15 22:50:23 -0700

    set up request npm package to call weatherstack api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,10 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=1eaf9e02b38ff7c781479b09c9cf35d9&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=1eaf9e02b38ff7c781479b09c9cf35d9&query=37.8267,-122.4233&units=f";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+  );
 });
