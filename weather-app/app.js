2a6a2cf3fce68055cbc565e3996b84050af50ee0
Jon <jong649@gmail.com>
2020-04-15 22:30:12 -0700

    make first api call to weatherstack.com for current conditions

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,9 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("2 Second Timer");
-}, 2000);
+const url =
+  "http://api.weatherstack.com/current?access_key=1eaf9e02b38ff7c781479b09c9cf35d9&query=37.8267,-122.4233";
 
-setTimeout(() => {
-  console.log("0 Second Timer");
-}, 0);
-
-console.log("Stopping");
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
