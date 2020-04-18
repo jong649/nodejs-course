8f85a3d2df393fa936db62ef935ed7c53154b624
Jon <jong649@gmail.com>
2020-04-17 21:16:40 -0700

    serving simple html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,28 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page");
+  res.send([
+    {
+      name: "Jon",
+      age: 38,
+    },
+    { name: "Tatyana", age: 37 },
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.send("<h1>About</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather page");
+  res.send({
+    forecast: "72 degrees",
+    location: "Room temperature",
+  });
 });
 
 app.listen(3000, () => {
