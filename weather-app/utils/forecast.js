1f7cf96e826a06bb6030e9c905874ba4294f6247
Jon <jong649@gmail.com>
2020-04-17 19:48:40 -0700

    destructured where possible

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=1eaf9e02b38ff7c781479b09c9cf35d9&query=${latitude},${longitude}&units=f`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather services!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
       callback(
         undefined,
-        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
       );
     }
   });
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -5,19 +5,16 @@ const geocode = (address, callback) => {
     address
   )}.json?access_token=pk.eyJ1Ijoiam9uZzY0OSIsImEiOiJjanpuODZoZDYwMThtM2Rqa3RwNXd0dmY3In0.Qp2uWC_yLxIJKVCG_TmOTA`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
-    } else if (
-      response.body.message === "Not Found" ||
-      response.body.features[0].length === 0
-    ) {
+    } else if (body.message === "Not Found" || body.features[0].length === 0) {
       callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
       });
     }
   });
