3644b30d4c7221e5942739d151d8e01295d09232
Jon <jong649@gmail.com>
2020-04-17 19:33:00 -0700

    playing with destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,26 @@
+const name = "Jon";
+const age = 38;
+
+const user = {
+  name,
+  age,
+  location: "Sacramento",
+};
+
+console.log(user);
+
+const product = {
+  label: "Pickles",
+  price: 5,
+  stock: 201,
+  salePrice: undefined,
+};
+
+const { label, price, stock, salePrice } = product;
+console.log(label);
+
+const transaction = (type, { label, stock }) => {
+  console.log(type, label, stock);
+};
+
+transaction("order", product);
