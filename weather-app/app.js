c4ad10c49a2a10e907fb5dc30e168b1c8ce6ae4d
Jon <jong649@gmail.com>
2020-04-17 18:58:53 -0700

    get location from command line arguments

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,12 +2,21 @@ const request = require("request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Boston", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const location = process.argv[2];
 
-forecast(-71.0596, 42.3605, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+if (location) {
+  geocode(location.toString(), (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+} else {
+  console.log("Please provide an address");
+}
