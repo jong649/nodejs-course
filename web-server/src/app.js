36165dd465995078b3a3afb655bda2aaaab9865c
Jon <jong649@gmail.com>
2020-04-17 23:22:51 -0700

    begin using handlebars to create dynamic content

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,31 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Jon Grant",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Jon Grant",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message:
+      "Here is some text pertaining to the help page. In the future, something more useful will be here.",
+    name: "Jon Grant",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
