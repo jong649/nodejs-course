793add66799067c9979bcfeae25e64275ea112c0
Jon <jong649@gmail.com>
2020-04-29 00:02:33 -0700

    playing with Promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,14 +1,35 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //resolve([1, 4, 7, 22, 554]);
-    reject("Things went wrong!");
-  }, 2000);
-});
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
 
-doWorkPromise
-  .then((result) => {
-    console.log("Success", result);
+// add(1, 2)
+//   .then((sum) => {
+//     console.log(sum);
+
+//     add(sum, 5)
+//       .then((sum2) => {
+//         console.log(sum2);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+add(1, 1)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
   })
   .catch((error) => {
-    console.log("Error", error);
+    console.log(error);
   });
